eab7067a8cb8d171c66b722389409a24c3d40420
Felipe Pêpe <felipepepe21@gmail.com>
2025-03-26 15:04:23 -0300

    refactor: :recycle: refactored player move

--- src/utils/playerMove.ts
+++ src/utils/playerMove.ts
@@ -10,41 +10,38 @@ const MAX_DISCARDS = 2;
 const playerMove = async (player: Player) => {
   let playerInput;
   let cardToDiscard;
-  let options;
+  const options = [1, 2];
 
   printHand(player);
 
   if (player.getDiscardCount() >= MAX_DISCARDS) {
-    options = [1];
     printWarning(
       "Max discards reached. You can't discard straight away at this round."
     );
-
-    console.log("\nMake a move.\n\t1 - Use a card");
+    cardToDiscard = await playerPlaceCard(player);
   } else {
-    options = [1, 2];
     console.log("\nMake a move.\n\t1 - Use a card\n\t2 - Discard a card");
+
+    do {
+      playerInput = await askQuestion("\nEnter your move » ");
+      if (options.includes(Number(playerInput))) {
+        // Selected discard
+        if (playerInput == 2) {
+          console.log("\nSelect the card you'll discard:\n");
+          cardToDiscard = await playerSelectCard(player);
+          player.addDiscardCount();
+        }
+        // Selected use card
+        else {
+          cardToDiscard = await playerPlaceCard(player);
+        }
+      } else {
+        console.log("■ Invalid option!");
+      }
+    } while (!options.includes(Number(playerInput)));
   }
 
-  do {
-    playerInput = await askQuestion("\nEnter your move » ");
-    if (options.includes(Number(playerInput))) {
-      // Selected discard
-      if (playerInput == 2) {
-        console.log("\nSelect the card you'll discard:\n");
-        cardToDiscard = await playerSelectCard(player);
-        player.addDiscardCount();
-        return cardToDiscard;
-      }
-      // Selected use card
-      else {
-        cardToDiscard = await playerPlaceCard(player);
-        return cardToDiscard;
-      }
-    } else {
-      console.log("■ Invalid option!");
-    }
-  } while (!options.includes(Number(playerInput)));
+  return cardToDiscard;
 };
 
 export default playerMove;
